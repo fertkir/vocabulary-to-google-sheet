de42dc62feb99a64eb039bdedbbfbbaa42678770
Kirill Fertikov <kirill.fertikov@gmail.com>
2019-12-15 00:04:46 +0300

    adding works for english

--- background.js
+++ background.js
@@ -1,19 +1,39 @@
-// background.js
+const API_KEY = 'AIzaSyCtq3pyX0R8jlaMVsrYeln_YRGY8LTGsyk';
+const DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+const SPREADSHEET_ID = '1TqaBgB4ijD0PnSLE0iawa1Kkm8Fi4Cm-52wXT-nnQc4';
+const SPREADSHEET_TAB_NAME = 'English';
 
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+function onGAPILoad() {
+  gapi.client.init({
+    apiKey: API_KEY,
+    discoveryDocs: DISCOVERY_DOCS,
   });
-});
+}
 
-// This block is new!
-chrome.runtime.onMessage.addListener(
+chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if( request.message === "open_new_tab" ) {
-      chrome.tabs.create({"url": request.url});
-    }
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+      gapi.auth.setToken({
+        'access_token': token,
+      });
+
+      const body = {values: [[
+        request
+      ]]};
+
+      gapi.client.sheets.spreadsheets.values.append({
+        spreadsheetId: SPREADSHEET_ID,
+        range: SPREADSHEET_TAB_NAME,
+        valueInputOption: 'USER_ENTERED',
+        resource: body
+      }).then(response => {
+        console.log(`${response.result.updates.updatedCells} cells appended.`)
+        sendResponse({success: true});
+      }, error => {
+        console.error(error);
+        sendResponse({success: false});
+      });
+    });
+    return true; // wait for response
   }
 );
--- content.js
+++ content.js
@@ -1,32 +1,30 @@
-// content.js
-// chrome.runtime.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-//     if( request.message === "clicked_browser_action" ) {
-//       var firstHref = $("div.example").eq(0).html();
+const targetWord = window.location.toString().split('/').pop().match(/\w+/g).toString();
+console.log('targetWord: ' + targetWord);
 
-//       console.log(firstHref);
+$("div.examp").each(function(index) {
+    $(this).append("&nbsp;<a class=\"saveLink\" href=\"javascript:void(0);\">[Save]</a>");
+});
 
-//       // This line is new!
-//       chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
-//     }
-//   }
-// );
+$(".saveLink").click(function() {
+    const self = $(this);
+    const exampleString = self.parent()
+        .text()
+        .replace(/\[(.*?)\]/g, "") // removing everything in brackets, including "[Save]"
+        .trim();
+	const stringWithMarkedWord = markTargetWord(exampleString);
+  	chrome.runtime.sendMessage(stringWithMarkedWord, function(response) {
+        console.log('Saving: "' + stringWithMarkedWord + '"');
+        if (response.success) {
+            self.remove();
+        } else {
+            console.error('Could not save: "' + stringWithMarkedWord + '"');
+        }
+  	});
+});
 
-    const targetWord = window.location.toString().split('/').pop().match(/\w+/g).toString();
-    console.log('targetWord: ' + targetWord)
-
-    $("div.examp").each(function(index) {
-  		$(this).append("&nbsp;<a class=\"saveLink\" href=\"javascript:void(0);\">[Save]</a>");
-	});
-
-   	$(".saveLink").click(function() {
-		const exampleString = $(this).parent().text().replace("[Save]", "").trim();
-  		console.log(markTargetWord(exampleString));
-	});
-
-	function markTargetWord(str) {
-		const word = targetWord.endsWith('y') 
-			? targetWord.substring(0, targetWord.length - 1) 
-			: targetWord;
-		return str.replace(new RegExp(word + "\\w*","g"), "*$&*");
-	}
+function markTargetWord(str) {
+    const word = targetWord.endsWith('y') 
+	   ? targetWord.substring(0, targetWord.length - 1) 
+	   : targetWord;
+	return str.replace(new RegExp(word + "\\w*","ig"), "*$&*");
+}
