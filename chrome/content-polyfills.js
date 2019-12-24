ae0a1b0725030b182914b8e7932313903039d089
Kirill Fertikov <kirill.fertikov@gmail.com>
2019-12-24 23:15:27 +0300

    refactored: eliminated duplicate code

---
+++ chrome/content-polyfills.js
@@ -0,0 +1,9 @@
+browser = {
+	runtime: {
+    	sendMessage: function(msg) {
+        	return new Promise(function(resolve, reject) {
+            	chrome.runtime.sendMessage(msg, resolve);
+            });
+        }
+    }
+}
--- firefox-android/background-polyfills.js
+++ firefox-android/background-polyfills.js
@@ -1,26 +1,7 @@
 const CLIENT_ID = '444973037518-hcoukn406meu93cufq0afsbh98k2tanb.apps.googleusercontent.com';
-const SPREADSHEET_ID = '1TqaBgB4ijD0PnSLE0iawa1Kkm8Fi4Cm-52wXT-nnQc4';
-const SPREADSHEET_TAB_NAME = 'English';
 
 let AUTH_TOKEN;
 let authDialogInitiatorTab;
-
-browser.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(request);
-
-    authorize().then(function(token) {
-      addLineToSheet(request, token).then(function(response) {
-        sendResponse({success: true});
-      }, function(error) {
-      	console.log(error);
-        sendResponse({success: false});
-      });
-    });
-
-    return true; // wait for response
-  }
-);
 
 async function authorize() {
   const redirectURL = "http://localhost/vocabulary-add/";
@@ -45,24 +26,6 @@ function tokenPromise() {
             setTimeout(waitForToken, 250);
         })();
     });
-}
-
-async function addLineToSheet(line, token) {
-  let url = `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/${SPREADSHEET_TAB_NAME}:append`;
-  url += `?valueInputOption=USER_ENTERED`;
-
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
-    },
-    body: JSON.stringify({values: [[ line ]]})
-  });
-  if (response.status !== 200) {
-  	throw new Error(response.statusText);
-  }
-  return response.json();
 }
 
 browser.webRequest.onBeforeRequest.addListener(function(requestDetails) {
