16316a2867bb68eb2d7f4df946bd3886d9c61b39
Kirill Fertikov <kirill.fertikov@gmail.com>
2019-12-25 00:29:31 +0300

    reduced manifest duplication

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ cd build
 # building firefox android extension
 cp -a ../firefox-android/. .build-firefox-android
 cp -a ../common/. .build-firefox-android
+jq -s '.[0] * .[1]' ../common/manifest.json ../firefox-android/manifest.json > .build-firefox-android/manifest.json
 mkdir firefox-android
 cd .build-firefox-android
 zip -r -1 ../firefox-android/test@example.com.xpi *
@@ -16,6 +17,7 @@ rm -r .build-firefox-android
 # building firefox desktop extension
 cp -a ../firefox/. .build-firefox
 cp -a ../common/. .build-firefox
+jq -s '.[0] * .[1]' ../common/manifest.json ../firefox/manifest.json > .build-firefox/manifest.json
 mkdir firefox-desktop
 cd .build-firefox
 zip -r -1 ../firefox-desktop/test@example.com.xpi *
@@ -25,4 +27,5 @@ rm -r .build-firefox
 # building chrome extension
 cp -a ../chrome/. .build-chrome
 cp -a ../common/. .build-chrome
+jq -s '.[0] * .[1]' ../common/manifest.json ../chrome/manifest.json > .build-chrome/manifest.json
 mv .build-chrome vocabulary-chrome-extension
