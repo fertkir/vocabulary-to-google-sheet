745be114d8a1d8906af83586dd6e81052279afd0
Kirill Fertikov <kirill.fertikov@gmail.com>
2020-09-19 18:54:28 +0300

    working in kiwi

--- kiwi/background-polyfills.js
+++ kiwi/background-polyfills.js
@@ -28,6 +28,29 @@ browser = {
             },
             getRedirectURL: chrome.identity.getRedirectURL
     },
+    tabs: {
+            query: function(params) {
+                return new Promise(function(resolve, reject) {
+                    chrome.tabs.query(params, resolve);
+                });
+            },
+            create: function(params) {
+                return new Promise(function(resolve, reject) {
+                    chrome.tabs.create(params, resolve);
+                });
+            },
+            remove: function(params) {
+                return new Promise(function(resolve, reject) {
+                    chrome.tabs.remove(params, resolve);
+                });
+            },
+            update: function(initiator, params) {
+                return new Promise(function(resolve, reject) {
+                    chrome.tabs.update(initiator, params, resolve);
+                });
+            }
+    },
+    webRequest: chrome.webRequest,
     i18n: chrome.i18n
 }
 
@@ -35,7 +58,7 @@ const AUTH_URL = buildAuthUrl();
 
 function buildAuthUrl() {
   const clientId = '444973037518-p9nbj088l23q3uhd9jnmbbu3lnn535mo.apps.googleusercontent.com';
-  const redirectURL = browser.identity.getRedirectURL();
+  const redirectURL = "http://localhost/vocabulary-add/";
   const scopes = ["https://www.googleapis.com/auth/spreadsheets"];
 
   let authURL = "https://accounts.google.com/o/oauth2/auth";
@@ -47,11 +70,39 @@ function buildAuthUrl() {
   return authURL;
 }
 
-authorize = function() {
-    return browser.identity.launchWebAuthFlow({
-        interactive: true,
-        url: AUTH_URL
-    }).then(function(redirectURL) {
-        return redirectURL.match(/access_token=([^&]+)&/)[1];
+let AUTH_TOKEN;
+let authDialogInitiatorTab;
+
+authorize = async function() {
+  authDialogInitiatorTab = await getCurrentTabId();
+  if (!AUTH_TOKEN) {
+    browser.tabs.create({ url: AUTH_URL });
+  }
+  return tokenPromise();
+}
+
+function tokenPromise() {
+    return new Promise(function (resolve, reject) {
+        (function waitForToken(){
+            if (AUTH_TOKEN) return resolve(AUTH_TOKEN);
+            setTimeout(waitForToken, 250);
+        })();
     });
+}
+
+browser.webRequest.onBeforeRequest.addListener(function(requestDetails) {
+  AUTH_TOKEN = requestDetails.url.match(/access_token=([^&]+)&/)[1];
+  closeCurrentTab();
+}, {urls: ["http://localhost/vocabulary-add/*"]});
+
+function closeCurrentTab() {
+  getCurrentTabId().then(function(currentTabId) {
+      browser.tabs.remove(currentTabId);
+  });
+  browser.tabs.update(authDialogInitiatorTab, {active: true});
+}
+
+async function getCurrentTabId() {
+  const tabs = await browser.tabs.query({currentWindow: true, active: true});
+  return tabs[0].id;
 }
