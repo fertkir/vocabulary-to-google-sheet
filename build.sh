c18c0d02640837c4ed02d3f808112b53daea8346
Kirill Fertikov <kirill.fertikov@gmail.com>
2020-10-30 00:16:20 +0300

    sourceforge publish automated

--- build.sh
+++ build.sh
@@ -1,16 +1,20 @@
+version=$(jq -r '.version' common/manifest.json)
+
 # cleaning
 rm -rf build
 
 mkdir build
 cd build
+mkdir develop
+mkdir -p release/$version
 
 # building firefox extension
 cp -a ../firefox/. .build-firefox
 cp -a ../common/. .build-firefox
 jq -s '.[0] * .[1]' ../common/manifest.json ../firefox/manifest.json > .build-firefox/manifest.json
-mkdir firefox
 cd .build-firefox
-zip -r -1 ../firefox/{7cadcc4b-af6c-4227-a247-7122bfeea911}.xpi *
+zip -r -1 ../develop/firefox.xpi *
+zip -r -1 ../release/$version/firefox-$version.xpi *
 cd ..
 rm -r .build-firefox
 
@@ -18,14 +22,18 @@ rm -r .build-firefox
 cp -a ../chrome/. .build-chrome
 cp -a ../common/. .build-chrome
 jq -s '.[0] * .[1]' ../common/manifest.json ../chrome/manifest.json > .build-chrome/manifest.json
-mv .build-chrome vocabulary-chrome-extension
+cd .build-chrome
+cp -R . ../develop/chrome
+zip -r -1 ../release/$version/chrome-$version.crx *
+cd ..
+rm -r .build-chrome
 
 # building kiwi extension
 cp -a ../kiwi/. .build-kiwi
 cp -a ../common/. .build-kiwi
 jq -s '.[0] * .[1]' ../common/manifest.json ../kiwi/manifest.json > .build-kiwi/manifest.json
-mkdir kiwi
 cd .build-kiwi
-zip -r -1 ../kiwi/vocabulary-to-spreadsheet.crx *
+zip -r -1 ../develop/kiwi.crx *
+zip -r -1 ../release/$version/kiwi-$version.crx *
 cd ..
 rm -r .build-kiwi
